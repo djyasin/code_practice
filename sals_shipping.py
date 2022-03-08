a767222200953566ffa5a46a7d7a7bdc3e56d184
Lila <djyasin.ly@gmail.com>
2022-03-07 19:45:46 -0500

    added statement for shipping type.

--- sals_shipping.py
+++ sals_shipping.py
@@ -1,31 +1,39 @@
-weight = 41.5
+weight = 4
 
 #ground shipping
 
 if weight <= 2:
-  print(weight * 1.50 + 20)
+    msg = str(weight * 1.50 + 20)
+    print("Ground shipping cost: $" + msg)
 elif ((weight>= 2) and (weight<= 6)):
-  print (weight * 3.50 + 20)
+    msg = str(weight * 3.50 + 20)
+    print("Ground shipping cost: $" + msg)
 elif ((weight>= 6) and (weight<= 10)):
-  print(weight * 4.00 + 20)
+    msg = str(weight * 4.00 + 20)
+    print("Ground shipping cost: $" + msg)
 elif weight > 10:
-  print( weight * 4.75 + 20)
+    msg = str( weight * 4.75 + 20)
+    print("Ground shipping cost: $" + msg)
 else:
-  print("error")
+    print("error")
 
 ground_shipping_premium = "$125.00"
 print("Ground Shipping Premium: " + ground_shipping_premium)
 
 #drone shipping
 if weight <= 2:
-   print(weight * 4.50)
+    msg = (weight * 4.50)
+    print("Drone shipping cost: $" + msg)
 elif ((weight>=2) and (weight<=6)):
-  print(weight * 9.00)
+    msg = str(weight * 9.00)
+    print("Drone shipping cost: $" + msg)
 elif ((weight>= 6) and (weight<=10)):
-  print(weight * 12.00)
+    msg = str(weight * 12.00)
+    print("Drone shipping cost: $" + msg)
 elif weight > 10:
-  print(weight * 10)
+    msg = str(weight * 10)
+    print("Drone shipping cost: $" + msg)
 else:
-  print("error")
+    print("error")
 
 #
